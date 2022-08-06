a06aaf234e1f5b7de0a7e12c20bf3dafba0ec154
Tushar Bhutani <tubhutani13@gmail.com>
2022-08-06 11:50:49 +0530

    Changed Method name and added comment for understanding in replace_regex

--- lib/replace_regex.rb
+++ lib/replace_regex.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
+# class String for replace vowels method
 class String
   REGEX_EXPR = /[aeiou]/.freeze
-  def replacestr
+  # method to replace vowels with *
+  def replace_vowels
     gsub(REGEX_EXPR, '*')
   end
 end
 if ARGV.length.zero?
   puts 'Please provide an input'
-else puts ARGV[0].replacestr
+else
+  puts ARGV[0].replace_vowels
 end
